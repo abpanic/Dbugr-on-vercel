19fcb90de199cdb6812f786c6fe9ac0e4237451d
Abhilash Panicker <abhilash.panicker@outlook.com>
2023-02-16 00:41:09 +0530

    Update BadgesCert.js

--- components/BadgesCert.js
+++ components/BadgesCert.js
@@ -1,23 +1,32 @@
-import certData from "@constants/badgeinfo";
+import userData from "@constants/data";
 import React from "react";
 
 export default function BadgesCert() {
   return (
     <section className="bg-white dark:bg-gray-800">
-      <div  data-iframe-width="150" data-iframe-height="270" className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
+      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
         <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
-          Badges
+          Blog: Migrating to vercel
         </h1>
-      
-      
-        
-          
-        <div  data-share-badge-id="90a3d6b2-1dad-4027-8979-1d6f9f222588" data-share-badge-host="https://www.credly.com"></div>
-        
-        <script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
-        </div>
+      </div>
     </section>
   );
 }
+
+const ExperienceCard = ({ title, desc, year, company, companyLink }) => {
+  return (
+    <div className="relative experience-card border p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4">
+      <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-200 font-bold dark:text-gray-800">
+        {year}
+      </h1>
+      <h1 className="font-semibold text-xl">{title}</h1>
+      <a href={companyLink} className="text-gray-500">
+        {company}
+      </a>
+      <p className="text-gray-600 dark:text-gray-400 my-2">{desc}</p>
+    </div>
+    
+  );
+};
 
 
