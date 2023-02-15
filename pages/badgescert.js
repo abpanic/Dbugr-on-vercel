682e461e4619d4df488b3d8f5915ca0812ab50ff
Abhilash Panicker <abhilash.panicker@outlook.com>
2023-02-16 00:44:32 +0530

    Update badgescert.js

--- pages/badgescert.js
+++ pages/badgescert.js
@@ -2,7 +2,7 @@ import BadgesCert from "../components/BadgesCert";
 import React from "react";
 import ContainerBlock from "../components/ContainerBlock";
 
-export default function BadgesCert() {
+export default function badgescert() {
   return (
     <ContainerBlock title="Badges and Certifications - Abhilash Panicker">
       <BadgesCert />
