8d1fe69719b283e72882f8113d78e690559aa1bf
Abhilash Panicker <abhilash.panicker@outlook.com>
2023-02-24 00:00:51 +0530

    Update BadgesCert.js

--- components/BadgesCert.js
+++ components/BadgesCert.js
@@ -6,7 +6,7 @@ export default function BadgesCert() {
     <section className="bg-white dark:bg-gray-800">
       <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
         <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
-          Badges and Certifications : ( To do: show content from <a href="https://www.credly.com/users/abhilash-panicker/badges"> </a>)
+          Badges and Certifications : ( To do: <a href="https://www.credly.com/users/abhilash-panicker/badges">Credly here</a>)
         </h1>
       </div>
     </section>
