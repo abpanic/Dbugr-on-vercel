e4e2a5369a8877a464bf4e40a24c2707a58631f9
Abhilash Panicker <abhilash.panicker@outlook.com>
2023-03-14 08:39:58 +0530

    Update BadgesCert.tsx

--- components/BadgesCert.tsx
+++ components/BadgesCert.tsx
@@ -5,12 +5,15 @@ import userData from "../constants/badgeinfo";
 export default function BadgesCert() {
   return (
     <section className="bg-white dark:bg-gray-800">
+      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
+        <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
+          Certifications and Badges
+        </h1>
+      </div>
       <div className="bg-[#F1F1F1] dark:bg-gray-900 px-4">
         <div className="pt-20 grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto gap-y-20 gap-x-20">
           {/* Social Buttons */}
           <div className="inline-flex flex-col">
-
-
             {/* Social Links */}
             <h1 className="text-xl font-semibold text-gray-700 mt-8 dark:text-gray-200">
               Links to my Certifications:
@@ -40,50 +43,34 @@ export default function BadgesCert() {
                   </div>
                 </a></div>
               </div>
-              <div className="flex flex-row justify-start items-center">
-                <div><a
-                  href="https://www.coursera.org/account/accomplishments/specialization/SWDBDGADF5D3"
-                  className="flex flex-row items-center space-x-4 group"
-                >
-                  <div className="my-4">&rarr;</div>
-                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
-                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
-                    Coursera
-                  </div>
-                </a></div>
-              </div>
             </div>
           </div>
-        
-        {/* Text area */}
-
-
-
-        <div><h3 className="bg-green-500 text-3xl rounded-md px-2 py-1 inline-block font-bold text-gray-50">    Credly Badges </h3>
         </div>
+      </div>
+      <div><h3 className="bg-green-500 text-3xl rounded-md px-2 py-1 inline-block font-bold text-gray-50">    Credly Badges </h3>
         <div className="grid grid-flow-col auto-cols-max gap-4 ">
           <div className="flex items-center gap-2 overflow-hidden">Updating the Credly section here</div>
           <div className="grid gap-4 grid-flow-row ">
 
 
-            <div className="grid gap-4 grid-flow-row h-20 w-20">IIT Roorkee Certification link: TO DO</div>
-            <div className="grid gap-4 grid-flow-row h-20 w-20">DataCamp link: To DO</div>   </div>
-          <div className="bg-[#F1F1F1] dark:bg-gray-900">
-            <div className="max-w-6xl mx-auto grid grid-cols-3 md:grid-cols-2 gap-4 py-20 pb-40">
-              {certData.accepted_badges.map((badg, idx) => (
-                <BadgeCard
-                  name={badg.name}
-                  link={"https://credly.com/badges/${badg.id}"}
-                  id={badg.id}
-                  image={badg.image}
-                  number={`${idx + 1}`}
-                />
-              ))}
+
+            <div className="bg-[#F1F1F1] dark:bg-gray-900">
+              <div className="max-w-6xl mx-auto grid grid-cols-3 md:grid-cols-2 gap-4 py-20 pb-40">
+                {certData.accepted_badges.map((badg, idx) => (
+                  <BadgeCard
+                    name={badg.name}
+                    link={"https://credly.com/badges/${badg.id}"}
+                    id={badg.id}
+                    image={badg.image}
+                    number={`${idx + 1}`}
+                  />
+                ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
+
     </section>
   );
 }
