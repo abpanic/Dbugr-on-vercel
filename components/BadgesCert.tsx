a5449a796f0af5a6e4c5586c684d78e4c90097e6
Abhilash Panicker <abhilash.panicker@outlook.com>
2023-03-14 07:09:45 +0530

    *updating badges link

--- components/BadgesCert.tsx
+++ components/BadgesCert.tsx
@@ -49,6 +49,7 @@ export default function BadgesCert() {
             <BadgeCard
               name={badg.name}
               link={"https://credly.com/badges/${badg.id}"}
+              image={badg.image}
               number={`${idx + 1}`}
             />
           ))}
@@ -75,14 +76,14 @@ export default function BadgesCert() {
 
 
 
-const BadgeCard = ({ name, link, number }) => {
+const BadgeCard = ({ name, link, image, number }) => {
   return (
     <a href={link} className="w-full block shadow-2xl">
       <div className="relative overflow-hidden">
         <div className="h-72 object-cover">
           <img
-            src={link}
-            alt="portfolio"
+            src={image}
+            alt={name}
             className="transform hover:scale-125 transition duration-2000 ease-out object-cover h-full w-full"
           />
         </div>
