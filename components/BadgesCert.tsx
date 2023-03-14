70ca4ba6a9bf87f635f8ab3275d5e13de22876c0
Abhilash Panicker <abhilash.panicker@outlook.com>
2023-03-14 08:50:59 +0530

    Update BadgesCert.tsx

--- components/BadgesCert.tsx
+++ components/BadgesCert.tsx
@@ -83,7 +83,7 @@ const BadgeCard = ({ name, link, image, number, id }) => {
           <img
             src={image}
             alt={name}
-            className="transform hover:scale-80 transition duration-2000 ease-out object-contain h-30 w-48"
+            className="transform hover:scale-80 transition duration-2000 ease-out object-contain h-20 w-30"
           />
         </div>
         <h1 className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
