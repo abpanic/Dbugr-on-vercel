0584631949bc3ea9e3f865de866ad8efdc5a461d
Abhilash Panicker <abhilash.panicker@outlook.com>
2023-03-19 22:54:02 +0530

    Update getLatestRepos.tsx
    *reverting back

--- lib/getLatestRepos.tsx
+++ lib/getLatestRepos.tsx
@@ -1,25 +1,36 @@
 import axios from "axios";
-import userData from '../constants/data';
 
-const getLatestRepos = async (data: { githubUsername: string }, token?:string) => {
-  
+const getLatestRepos = async (data, token) => {
+  // console.log("data", data);
   try {
-    const username = userData.githubUsername;
-    const tokenHeader = token? {Authorization: `token ${process.env.GITHUB_AUTH_TOKEN}` } : {} ;
+    const username = data.githubUsername;
 
-    const res = await axios.get(
+    let token = `token ${process.env.GITHUB_AUTH_TOKEN}`;
+    // console.log("TOKEN", token);
+
+    if (token) {
+      const res = await axios.get(
         `https://api.github.com/search/repositories?q=user:${username}+sort:author-date-asc`,
         {
-          headers: tokenHeader,
+          headers: {
+            Authorization: `token ${token}`,
+          },
         }
       );
-      const repos = res.data?.items??[];
-      const latest5Repos = repos.slice(0, 5);
+      let repos = res.data.items;
+      let latest5Repos = repos.splice(0, 5);
       
       return latest5Repos;
+    } else {
+      const res = await axios.get(
+        `https://api.github.com/search/repositories?q=user:${username}+sort:author-date-asc`
+      );
+      let repos = res.data.items;
+      let latest5Repos = repos.splice(0, 5);
+      return latest5Repos;
+    }
   } catch (err) {
     console.log(err);
-    return[];
   }
 };
 
