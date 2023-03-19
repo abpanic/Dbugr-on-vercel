50f596e3712740c953c7952b77595c20260518f8
Abhilash Panicker <abhilash.panicker@outlook.com>
2023-03-19 23:06:15 +0530

    **imported userdata in latestrepro and removed next.config.mjs
    **imported userdata in latestrepro
    **removed next.config.mjs to fix 404

--- lib/getLatestRepos.tsx
+++ lib/getLatestRepos.tsx
@@ -1,36 +1,27 @@
 import axios from "axios";
+import userData from "../constants/data";
 
 const getLatestRepos = async (data, token) => {
-  // console.log("data", data);
   try {
-    const username = data.githubUsername;
+    const username = userData.githubUsername;
 
-    let token = `token ${process.env.GITHUB_AUTH_TOKEN}`;
-    // console.log("TOKEN", token);
+    // Use the provided token, or fallback to the environment variable
+    token = token ?? process.env.GITHUB_AUTH_TOKEN;
 
-    if (token) {
-      const res = await axios.get(
-        `https://api.github.com/search/repositories?q=user:${username}+sort:author-date-asc`,
-        {
-          headers: {
-            Authorization: `token ${token}`,
-          },
-        }
-      );
-      let repos = res.data.items;
-      let latest5Repos = repos.splice(0, 5);
-      
-      return latest5Repos;
-    } else {
-      const res = await axios.get(
-        `https://api.github.com/search/repositories?q=user:${username}+sort:author-date-asc`
-      );
-      let repos = res.data.items;
-      let latest5Repos = repos.splice(0, 5);
-      return latest5Repos;
-    }
+    const headers = token ? { Authorization: `token ${token}` } : {};
+
+    const res = await axios.get(
+      `https://api.github.com/search/repositories?q=user:${username}+sort:author-date-asc`,
+      { headers }
+    );
+
+    const repos = res.data.items ?? [];
+    const latest5Repos = repos.slice(0, 5);
+
+    return latest5Repos;
   } catch (err) {
     console.log(err);
+    return [];
   }
 };
 
