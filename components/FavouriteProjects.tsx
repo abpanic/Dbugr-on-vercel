95cd2bf08bb5e53fe678b25bc51e2cc27c5367df
Abhilash Panicker <abhilash.panicker@outlook.com>
2023-05-02 18:42:56 +0530

    added correct links

--- components/FavouriteProjects.tsx
+++ components/FavouriteProjects.tsx
@@ -36,7 +36,7 @@ export default function FavouriteProjects() {
         <div className="grid md:grid-cols-3 gap-8 lg:-mt-8 pb-40">
           {/* Single card */}
           <a
-            href="https://webdevtutorials101.netlify.app/"
+            href="https://abpanic.github.io/Task-Tracker/"
             className="w-full block col-span-3 shadow-2xl"
           >
             <div className="relative overflow-hidden">
@@ -55,7 +55,7 @@ export default function FavouriteProjects() {
           </a>
           {/* Single card */}
           <a
-            href="https://placeholdertech.in"
+            href="https://webdevtutorials101.netlify.app/"
             className="w-full block col-span-3  sm:col-span-2 shadow-2xl"
           >
             <div className="relative overflow-hidden">
