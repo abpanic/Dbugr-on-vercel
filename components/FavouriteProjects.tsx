e218cbe516d6bc822e37bf88be89adf5df09a93b
Abhilash Panicker <abhilash.panicker@outlook.com>
2023-05-02 18:22:40 +0530

    updating current project

--- components/FavouriteProjects.tsx
+++ components/FavouriteProjects.tsx
@@ -41,12 +41,12 @@ export default function FavouriteProjects() {
           >
             <div className="relative overflow-hidden">
               <img
-                src="/webdev101.png"
-                alt="WebDev101"
+                src="/TaskTracker.png"
+                alt="Task Tracker"
                 className="transform hover:scale-125 transition duration-2000 ease-out"
               />
               <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                Web Dev 101 for Onboarding SDK engineers
+                Time Tracker for task driven time management based on Pomodoro Technique
               </h1>
               <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
                 01
@@ -61,12 +61,12 @@ export default function FavouriteProjects() {
             <div className="relative overflow-hidden">
               {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
               <img
-                src="/placeholdertech.png"
-                alt="portfolio"
+                src="/webdev101.png"
+                alt="Web Dev 101"
                 className="transform hover:scale-125 transition duration-2000 ease-out"
               />
               <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                PlaceholderTech
+              Web Dev 101 for Onboarding SDK engineers
               </h1>
               <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
                 02
