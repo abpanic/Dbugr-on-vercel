09fef46ff3218e305fe5137911e93b77b3511a2c
Abhilash Panicker <abhilash.panicker@outlook.com>
2023-05-02 18:58:25 +0530

    corrected path for task tracker

--- components/FavouriteProjects.tsx
+++ components/FavouriteProjects.tsx
@@ -41,7 +41,7 @@ export default function FavouriteProjects() {
           >
             <div className="relative overflow-hidden">
               <img
-                src="/TaskTracker.png"
+                src="/TaskTracker"
                 alt="Task Tracker"
                 className="transform hover:scale-125 transition duration-2000 ease-out"
               />
