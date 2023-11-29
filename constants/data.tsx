f0ea63804c0c536c335802252971259f9f6f6ad2
Abhilash <abhilash.panicker@outlook.com>
2023-11-29 22:47:02 +0530

    experience overhaul

--- constants/data.tsx
+++ constants/data.tsx
@@ -18,120 +18,126 @@ const userData = {
     currentProjectUrl: "https://abpanic.github.io/Task-Tracker/",
   },
   experience: [
-    {
-      title: "Partner Technical Advisor (Sonata Software)",
-      company: "Microsoft",
-      year: "2019 - present",
-      companyLink: "https://microsoft.com",
-      desc:[ "Serving in Program Management role for Customer Experience",
-        "	Led a team of 27 engineers, 3 Technical Leads for diverse customer base across 2 continents with delivery Partner Sonata Software.",
-        "	Upskilling Engineers Azure DevOps and GitHub for CI/CD operations, Custom code debugging to work on 100% of the volume in-house.",
-        "	Developed and implemented data-driven analysis for support topics, resulting in increased focus and reduced escalations from Tier 1 team by 11%. ",
-        "	Built high skill SME team (internal Tier 2) at Partner location working exclusively working on Strategic , Escalations and Premier(for fee) volumes enabling the team to take 60% of the volume.",
-        "	Successfully implemented Agile Change Management for continuously evolving cloud landscape while still maintaining SLAs.",
-        "	Project Management for projects time to time for case hygiene, new feature roll-out, etc. for delivery on time and on budget.",
-        "	 Projects on upskilling for Partner and SDK Engineers : D365 SDK Tutorials (webdevtutorials101.netlify.app) ",
-        
-        "Proactive Support/Advisory Service:",
-        "	Advisory for Strategic Customers workload migration, solution lifecycle management for Digital Transformation Platform.",
-        "	Collaborated with ISVs (Independent Software Vendors), MSP (Managed Service Providers), and SIs (System Integrators) to deliver high-impact solutions on the Dynamics platform with assistance in best practice to list in Microsoft AppSource.",
-        "	Bug tracking portal for customers to track their bug status and get email alert when released resulting in 100% improvement in case closure on bug cases.",
-            
-        ]
-    },
-    {
-      title: "Support Escalation Engineer",
-      company: "Microsoft",
-      year: "2018 - 2019",
-      companyLink: "https://microsoft.com",
-      desc:["Served as an escalation point for Tier 1/vendor team and FTE/Tier 2 team.",
-      "	Managed high visibility cases and reduced backlog for Tier 1 and FTE team, resulting in a 15% reduction in case resolution time.",
-      "	Analyzed customers' technical environments and anticipated potential challenges by communicating effectively with customer contacts to influence customer business outcomes, resulting in a 7% improvement in customer satisfaction ratings.",
-      "	Set success criteria and continually analyzed and optimized the plan to achieve the best results, maintaining stable Experience scores of 4.7/5 for vendor teams in the POD.", 
-      "	Communicated complex technical issues to and established credibility with a variety of audiences, resulting in a 20% improvement in issue resolution time.",
-      ] 
-    },
-    {
-      title: "Support Engineer (Premier & Partner Support)",
-      company: "Microsoft",
-      year: "2016 - 2018",
-      companyLink: "https://microsoft.com",
-      desc:["Served as primary contact for Paid support.",
+  {
+    title: "Partner Technical Advisor (Sonata Software)",
+    company: "Microsoft",
+    year: "2019 - present",
+    companyLink: "https://microsoft.com",
+    desc: [
+      "Serving in Program Management role for Customer Experience",
+      "Led a team of 27 engineers, 3 Technical Leads for diverse customer base across 2 continents with delivery Partner Sonata Software.",
+      "Upskilling Engineers Azure DevOps and GitHub for CI/CD operations, Custom code debugging to work on 100% of the volume in-house.",
+      "Developed and implemented data-driven analysis for support topics, resulting in increased focus and reduced escalations from Tier 1 team by 11%.",
+      "Built high skill SME team (internal Tier 2) at Partner location working exclusively working on Strategic, Escalations and Premier(for fee) volumes enabling the team to take 60% of the volume.",
+      "Successfully implemented Agile Change Management for continuously evolving cloud landscape while still maintaining SLAs.",
+      "Project Management for projects time to time for case hygiene, new feature roll-out, etc. for delivery on time and on budget.",
+      "Projects on upskilling for Partner and SDK Engineers: D365 SDK Tutorials (webdevtutorials101.netlify.app)",
+      "Proactive Support/Advisory Service:",
+      "Advisory for Strategic Customers workload migration, solution lifecycle management for Digital Transformation Platform.",
+      "Collaborated with ISVs (Independent Software Vendors), MSP (Managed Service Providers), and SIs (System Integrators) to deliver high-impact solutions on the Dynamics platform with assistance in best practice to list in Microsoft AppSource.",
+      "Bug tracking portal for customers to track their bug status and get email alert when released resulting in 100% improvement in case closure on bug cases.",
+    ]
+  },
+  {
+    title: "Support Escalation Engineer",
+    company: "Microsoft",
+    year: "2018 - 2019",
+    companyLink: "https://microsoft.com",
+    desc: [
+      "Served as an escalation point for Tier 1/vendor team and FTE/Tier 2 team.",
+      "Managed high visibility cases and reduced backlog for Tier 1 and FTE team, resulting in a 15% reduction in case resolution time.",
+      "Analyzed customers' technical environments and anticipated potential challenges by communicating effectively with customer contacts to influence customer business outcomes, resulting in a 7% improvement in customer satisfaction ratings.",
+      "Set success criteria and continually analyzed and optimized the plan to achieve the best results, maintaining stable Experience scores of 4.7/5 for vendor teams in the POD.",
+      "Communicated complex technical issues to and established credibility with a variety of audiences, resulting in a 20% improvement in issue resolution time.",
+    ]
+  },
+  {
+    title: "Support Engineer (Premier & Partner Support)",
+    company: "Microsoft",
+    year: "2016 - 2018",
+    companyLink: "https://microsoft.com",
+    desc: [
+      "Served as primary contact for Paid support.",
       "Work exclusively on paid support and Tier 1 escalation cases with Days to Solution of 15 days",
       "Drove discussions on cloud governance and compliance as needed with Account Management Team, resulting in a 2% reduction in future escalations.",
       "Collaborated with Partners and Strategic customers to drive discussions on cloud governance and compliance, resulting in a 10% increase in customer adoption of Microsoft cloud solutions.",
-      ] 
-    },
-    {
-      title: "Technical Consultant",
-      company: "Wipro",
-      year: "2014-2016",
-      companyLink: "https://wipro.com",
-      desc: ["Technical Lead for Microsoft Dynamics CRM (Cloud operations)",
-      "Defining and creating OKR (Objectives and key results) and KPIs (Key Performance Indicators) for Tier 1 to support  created",
+    ]
+  },
+  {
+    title: "Technical Consultant",
+    company: "Wipro",
+    year: "2014-2016",
+    companyLink: "https://wipro.com",
+    desc: [
+      "Technical Lead for Microsoft Dynamics CRM (Cloud operations)",
+      "Defining and creating OKR (Objectives and key results) and KPIs (Key Performance Indicators) for Tier 1 to support created",
       "Agile based POD methodology to reduce Days to Solution of cases by 23%",
       "Reduced the number of escalations to Tier 2 to 0.25% from 4% by actively identifying escalation triggers.",
       "Model and coach, the team to customer focused team achieving Days to Solution of 1.4 days an improvement from historical 7 days.",
-      "Improvement in solution time by 37% leveraging Tier 2 to create focus SME for each topic."]
-    },
-    {
-      title: "Technical Associate",
-      company: "IKS Health(formerly Inventurus Knowledge Solutions)",
-      year: "2013- 2014",
-      companyLink: "https://www.ikshealth.com",
-      desc: ["Data Engineer for new client entity: Physiotherapy Associates, US that consisted of 700 clinics.",
+      "Improvement in solution time by 37% leveraging Tier 2 to create focus SME for each topic."
+    ]
+  },
+  {
+    title: "Technical Associate",
+    company: "IKS Health(formerly Inventurus Knowledge Solutions)",
+    year: "2013- 2014",
+    companyLink: "https://www.ikshealth.com",
+    desc: [
+      "Data Engineer for new client entity: Physiotherapy Associates, US that consisted of 700 clinics.",
       "Built Minimum Viable Product (MVP) from inception through to productization for a new client entity consisting of 700 clinics, resulting in 100% adoption of the product.",
-      "Built v1 product  for following:",
+      "Built v1 product for following:",
       "Store Keeper: Inventory data stored in SQL DB with access based on authentication",
       "Appointment Scheduler: App to schedule appointments to clinics for patients with API endpoints to allow future integrations.",
       "Database Unification: Migrated legacy data stored in MS SQL, MySQL and Excel sheet of almost 700 clinics to the new parent entity’s database."
-  ]
-    },
-    {
-      title: "Customer Care Representative",
-      company: "Sitel",
-      year: "2011 - 2013",
-      companyLink: "https://www.sitel.com",
-      desc: ["Support Engineer for Microsoft SQL paid support.",
-    "Develop scripts, perform database support activities, and conduct performance tuning as part of troubleshooting with 87% CSAT.",
     ]
-    },
-    {
-      title: "Proprietor",
-      company: "FinaFrontier",
-      year: "2008 - 2010",
-      companyLink: "https://www.finafrontier.in/",
-      desc: ["CRM and ERP solution to small and medium enterprises in India.",
+  },
+  {
+    title: "Customer Care Representative",
+    company: "Sitel",
+    year: "2011 - 2013",
+    companyLink: "https://www.sitel.com",
+    desc: [
+      "Support Engineer for Microsoft SQL paid support.",
+      "Develop scripts, perform database support activities, and conduct performance tuning as part of troubleshooting with 87% CSAT.",
+    ]
+  },
+  {
+    title: "Proprietor",
+    company: "FinaFrontier",
+    year: "2008 - 2010",
+    companyLink: "https://www.finafrontier.in/",
+    desc: [
+      "CRM and ERP solution to small and medium enterprises in India.",
       "Bootstrapped business with 4000 INR",
-      "Interacted with business in Mumbai, Jalna, Aurangabad MIDCs", 
+      "Interacted with business in Mumbai, Jalna, Aurangabad MIDCs",
       "The business was sold after nearly 2 years for 5 lakhs.",
-      "(This was before the rise of high bandwidth cheap Internet and cheap smartphones. Selling software through CD/DVD did not work well due to piracy)",]
-    },
-  ],
-  education: [
-    {
-      title: "M.S. - Data Science",
-      company: "University of Colorado Boulder",
-      year: "2021",
-      companyLink: "https://www.colorado.edu/",
-      desc: "Data Science is my speciality with additional learning in Statistics and Software Engineering.",
-    },
-    {
-      title: "Bachelor of Computer Application",
-      company: "Jaipur National University, Jaipur.",
-      year: "2019",
-      companyLink: "https://www.jnujaipur.ac.in/",
-      desc: "Thanks to better time management skills, I had enough time to complete this along with my full-time job ",
-    },
-    {
-      title: "Bachelor of Technology - Electrical",
-      company: "Rajasthan Technical University",
-      year: "2006",
-      companyLink: "https://www.rtu.ac.in/index/",
-      desc: "Quit in 2008 to start my business. Built CRM and ERP software and seeling to small and medium business across Mumbai, Sangli, Jalna, Ahmedabad.",
-    },
-
-  ],
+      "(This was before the rise of high bandwidth cheap Internet and cheap smartphones. Selling software through CD/DVD did not work well due to piracy)",
+    ]
+  },
+],
+education: [
+  {
+    title: "M.S. - Data Science",
+    company: "University of Colorado Boulder",
+    year: "2021",
+    companyLink: "https://www.colorado.edu/",
+    desc: "Data Science is my speciality with additional learning in Statistics and Software Engineering.",
+  },
+  {
+    title: "Bachelor of Computer Application",
+    company: "Jaipur National University, Jaipur.",
+    year: "2019",
+    companyLink: "https://www.jnujaipur.ac.in/",
+    desc: "Thanks to better time management skills, I had enough time to complete this along with my full-time job",
+  },
+  {
+    title: "Bachelor of Technology - Electrical",
+    company: "Rajasthan Technical University",
+    year: "2006",
+    companyLink:"https://www.rtu.ac.in/index/",
+    desc: "Quit in 2008 to start a business. Addressing digital needs of small enterprises across Mumbai, Sangli, Jalna, Ahmedabad.",
+  },
+],
 
   currentlearning: [
     {
